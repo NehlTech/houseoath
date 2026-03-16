5dc6e76733fb4250c22fb48de9459e92ba933363
nehltech <obololastkiller@gmail.com>
2026-03-16 09:12:45 +0000

    fix: switch to html2canvas-pro for oklab color support (Tailwind v4)

--- src/components/tabs/ReceiptPreviewModal.tsx
+++ src/components/tabs/ReceiptPreviewModal.tsx
@@ -2,7 +2,7 @@
 
 import { useRef, useState, useEffect, useCallback } from 'react';
 import { createPortal } from 'react-dom';
-import html2canvas from 'html2canvas';
+import html2canvas from 'html2canvas-pro';
 import { Client, Payment } from '@/context/StudioContext';
 
 interface ReceiptPreviewModalProps {
